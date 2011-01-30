a06993013ac8bf8c2c2a0b185bcd27c4e5b829bd
Syd Lawrence <syd@sydlawrence.com>
2011-01-30 17:30:08 +0000

    Added github link

--- code.php
+++ code.php
@@ -1,6 +1,6 @@
 <?php
 $title = "The Code";
-$content = "<p>You can download the code from <a href=''>github</a></p>";
+$content = "<p>You can download the code from <a href='https://github.com/sydlawrence/HTML5-History'>github</a></p>";
 
 
 if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
